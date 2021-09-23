27fb16b134cb56b2cf313d05b08ebefaa03ee84b
aleynaerkisi <alyeynaerkisi@gmail.com>
2021-09-23 19:15:19 +0300

    Section 9

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -68,6 +68,7 @@ $route = Route::current()->getName();
           </a>
           <ul class="treeview-menu">
             <li><a href="{{ route('student.class.view') }}"><i class="ti-more"></i>Student Class</a></li>
+            <li><a href="{{ route('student.year.view') }}"><i class="ti-more"></i>Student Year</a></li>
           </ul>
         </li>
 		
