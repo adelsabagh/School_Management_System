e047c5ad0d69a16f4e24615cde44593a75a40c61
aleynaerkisi <alyeynaerkisi@gmail.com>
2021-09-27 14:06:38 +0300

    section 13

--- app/Models/FeeCategoryAmount.php
+++ app/Models/FeeCategoryAmount.php
@@ -11,4 +11,9 @@ class FeeCategoryAmount extends Model
     {
         return $this->belongsTo(FeeCategory::class,'fee_category_id','id');
     }
+
+    public function student_class()
+    {
+        return $this->belongsTo(StudentClass::class,'class_id','id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -151,4 +151,11 @@ Route::prefix('setups')->group(function () {
     Route::get('fee/amount/add', [FeeAmountController::class, 'AddFeeAmount'])->name('fee.amount.add');
 
     Route::post('fee/amount/store', [FeeAmountController::class, 'StoreFeeAmount'])->name('store.fee.amount');
+
+    Route::get('fee/amount/edit/{fee_category_id}', [FeeAmountController::class, 'EditFeeAmount'])->name('fee.amount.edit');
+
+    Route::post('fee/amount/update/{fee_category_id}', [FeeAmountController::class, 'UpdateFeeAmount'])->name('update.fee.amount');
+
+    Route::get('fee/amount/details/{fee_category_id}', [FeeAmountController::class, 'DetailsFeeAmount'])->name('fee.amount.details');
+
 });
